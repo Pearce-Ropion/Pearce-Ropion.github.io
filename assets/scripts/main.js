685a281df2444e8ea9187b460ff67ec89d840b8c
Pearce <pearce.ropion@me.com>
2018-01-22 18:45:02 -0800

    About Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,43 @@
 $(document).ready(function() {
-  const isMobile = {
-    Android: function() {
-      return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-      return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-      return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-      return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-  };
+    var smoothScroll = function(target) {
+        $('body, html').animate({ 'scrollTop': target.offset().top + 50 }, 500);
+    };
 
-  // turn off parallax effect on mobile devices
-  if (!isMobile.any()) {
-    skrollr.init({
-      render: function(data) {
-        //Debugging - Log the current scroll position.
-        // console.log(data.curTop);
-      },
-      smoothScrolling: false,
-      forceHeight: false
+    $('.scroll-down').on('click', function(ev) {
+        ev.preventDefault();
+        smoothScroll($(this.hash));
     });
-  }
+
+    const isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    // turn off parallax effect on mobile devices
+    if (!isMobile.any()) {
+        skrollr.init({
+            render: function(data) {
+                //Debugging - Log the current scroll position.
+                // console.log(data.curTop);
+            },
+            smoothScrolling: false,
+            forceHeight: false
+        });
+    }
 });
