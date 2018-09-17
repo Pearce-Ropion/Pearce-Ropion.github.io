da197d22551c5ef63df5a413ee765d3c1322c078
Pearce <pearce.ropion@me.com>
2018-09-17 15:42:41 -0700

    Skills page

--- _scripts/main.js
+++
@@ -1,8 +0,0 @@
-var smoothScroll = function(target) {
-    $('body, html').animate({ 'scrollTop': target.offset().top + 50 }, 500);
-}
-
-$('.scroll-down').on(click, function(ev)) {
-    ev.preventDefault();
-    smoothScroll($(this.hash));
-}
